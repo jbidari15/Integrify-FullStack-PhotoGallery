7ddd2aab253b5e4a8f273bdfadd168399f8ce4b7
Jaya Kumar Bidari <jaya.bidari@metropolia.fi>
2018-08-08 11:46:58 +0300

    app.js in backend refactored for heroku hosting

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use("/uploads", express.static("uploads"));
 
 const studentRoutes = require("./routes/students.js");
 app.use("/integrify/students", studentRoutes);
+app.use(express.static("integrify-gallery-front-end/build"));
+app.get("*", (req, res) =>
+  res.send(path.join(__dirname, "integrify-gallery-front-end/build/index.html"))
+);
 
 //serve static assets for production
 if (process.env.Node_ENV === "production") {
